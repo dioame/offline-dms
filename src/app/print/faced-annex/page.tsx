2dca905740dfbe0aeba9a8bbc11cd0a6ecf7975e
dioamejade@gmail.com <dioamejade@gmail.com>
2026-06-25 10:26:49 +0800

    updates-faced-records

--- src/app/print/faced-annex/page.tsx
+++ src/app/print/faced-annex/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import "@/styles/print-faced.css";
 import { Suspense, useEffect, useMemo, useState } from "react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
